080f6fd6eea4cef280febe2a970fd0e9c4b7abc8
JoaoDias61 <jpdias641@gmail.com>
2023-06-24 10:21:08 -0300

    added test

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.model.calculateTransformerInformation import calculateTransformerInformation
+from src.model.riskMatrix import riskMatrix
 from src.model.filterData import filterData
 
 from dotenv import load_dotenv
+from pydantic import BaseModel
 from fastapi import FastAPI
 import pyodbc
 import os
@@ -20,14 +22,43 @@ cursor = conn.cursor()
 
 app = FastAPI()
 
+class CalculateInput(BaseModel):
+    codigo_operacional: str
+    familia: str
+
 @app.get("/filterData")
 def get_data():
     data_processor = filterData(cursor)
     data = data_processor.process_data()
     grouped_data = data.groupby('nomeFamilia')['codigoOperacional'].apply(list).reset_index()
-    return grouped_data.to_dict(orient='records')
+    result = grouped_data.to_dict(orient='records')
+    info_adicional = {
+        "subsistemas": [
+            "Parte Ativa",
+            "Comutador sobrecarga",
+            "Parte Ativa",
+            "Acessórios",
+            "Ambiente",
+            "Tanque",
+            "Resfriamento",
+            "Preservação do Óleo Isolante",
+            "Bucha"
+        ]
+    }
+    result.append(info_adicional)
+    return result
+
+@app.post("/riskMatrix/")
+async def calculate_transformer_information(data: CalculateInput):
+          codigo_operacional = data.codigo_operacional
+          familia = data.familia
+          resultado = riskMatrix(cursor, codigo_operacional, familia).processRiskMatrix()
+          return resultado.to_dict(orient='list')
+
 
 @app.post("/calculateTransformerInformation/")
-async def calculate_transformer_information(codigo_operacional: str, familia: str):
+async def calculate_transformer_information(data: CalculateInput):
+          codigo_operacional = data.codigo_operacional
+          familia = data.familia
           resultado = calculateTransformerInformation(cursor, codigo_operacional, familia).executar_calculo()
           return resultado.to_dict()
---
+++ src/model/riskMatrix.py
@@ -0,0 +1,73 @@
+import pandas as pd
+
+
+class riskMatrix:
+    def __init__(self, cursor, codigo_operacional, familia):
+        self.cursor = cursor
+        self.codigo_operacional = codigo_operacional
+        self.familia = familia
+
+    def processRiskMatrix(self):
+        query = '''
+        SELECT DISTINCT
+                      Calculo.Codigo AS CodigoCalculo
+                    , ExecucaoCalculoResultado.Resultado AS ResultadoCalculo 
+                    , MAX(EntradaVariavel.UltimaAtualizacao) AS UltimaAtualizacaoCalculo
+                    , ie.Descricao AS descricaoInstalacaoEletrica
+                    , e.Descricao AS descricaoEquipamento
+                    , EntradaVariavel.Revisao As RevisaoVariavel 
+                    , Variavel.Codigo As VariavelCodigo
+                    , Variavel.Descricao As VariavelDescricao
+                    , EntradaVariavel.Valor As ResultadoVariavel 
+                    , MAX(EntradaVariavel.DataMedicao) AS dataMedicaoVariavel
+
+                FROM [EngineCalculo].[CalculoResultado]  As ExecucaoCalculoResultado
+                    INNER JOIN [EngineCalculo].[CalculoResultadoEquipamento] 
+                        ON CalculoResultadoEquipamento.CalculoId = ExecucaoCalculoResultado.CalculoId 
+                        AND CalculoResultadoEquipamento.JobId = ExecucaoCalculoResultado.JobId
+                    INNER JOIN [Treetech].[ViewEngine_UltimoJob] 
+                        ON ViewEngine_UltimoJob.EquipamentoId = CalculoResultadoEquipamento.EquipamentoId 
+                        AND ViewEngine_UltimoJob.JobId = ExecucaoCalculoResultado.JobId
+                    INNER JOIN [EngineCalculo].[Calculo] 
+                        ON Calculo.Id = ExecucaoCalculoResultado.CalculoId
+                    INNER JOIN [EngineCalculo].[CalculoResultadoVariavel] As EntradaVariavel 
+                        ON ExecucaoCalculoResultado.JobId = EntradaVariavel.JobId
+                    INNER JOIN [EngineCalculo].[RevisaoVariavel] As RevisaoVariavel 
+                        ON RevisaoVariavel.VariavelId = EntradaVariavel.VariavelId 
+                        AND RevisaoVariavel.Revisao = EntradaVariavel.Revisao
+                    INNER JOIN [EngineCalculo].[Variavel] 
+                        ON Variavel.Id = EntradaVariavel.VariavelId 
+                    INNER JOIN Equipamento AS e 
+                        ON e.Id = CalculoResultadoEquipamento.EquipamentoId
+                    INNER JOIN EquipamentoInstalacaoEletrica AS eie 
+                        ON e.Id = eie.EquipamentoId
+                    INNER JOIN InstalacaoEletrica AS ie 
+                        ON ie.Id = eie.InstalacaoEletricaId
+                    INNER JOIN Familia AS f 
+                        ON f.TucId = e.TucId
+
+                WHERE 1 = 1
+                    AND f.Nome = ?
+                    AND e.CodigoOperacional = ?
+                    AND RevisaoVariavel.TipoOrigem = 4 --Variágel de origem do ECM
+                    AND EntradaVariavel.DataMedicao > '2023-05-29 23:59:59' 
+                    AND e.EquipamentoSigmaId is not null 
+
+                GROUP BY Calculo.Codigo, ExecucaoCalculoResultado.Resultado, ie.Descricao, e.Descricao, EntradaVariavel.Revisao, Variavel.Codigo, Variavel.Descricao, EntradaVariavel.Valor
+
+                ORDER BY ie.Descricao,
+                        e.Descricao,
+                        MAX(EntradaVariavel.DataMedicao) DESC,
+                        ExecucaoCalculoResultado.Resultado DESC
+        '''
+        self.cursor.execute(query, self.familia, self.codigo_operacional)
+        resultado_sql = self.cursor.fetchall()
+
+        colunas = ['CodigoCalculo', 'ResultadoCalculo', 'UltimaAtualizacaoCalculo', 'descricaoInstalacaoEletrica',
+                   'descricaoEquipamento', 'RevisaoVariavel', 'VariavelCodigo', 'VariavelDescricao', 'ResultadoVariavel', 'dataMedicaoVariavel']
+
+
+        dados = [dict(zip(colunas, row)) for row in resultado_sql]
+        df = pd.DataFrame(dados)
+
+        return df
